42b5bb839d4e608efab574e66682703cdb8aa092
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2014-07-09 12:55:13 -1000

    ajout alias dans .bashrc

--- scripts/base.sh
+++ scripts/base.sh
@@ -5,4 +5,6 @@ apt-get -y install linux-headers-$(uname -r)
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
+grep -qs autocd /root/.bashrc || echo "shopt -s auto\nalias l='ls -al'" >>/root/.bashrc
+
 echo "UseDNS no" >> /etc/ssh/sshd_config
