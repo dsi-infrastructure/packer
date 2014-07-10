e8ced62598131b930c6d95431c1634c8a032e785
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-07-10 02:17:16 -1000

    Fusion du script d'installation de cloudinit.

--- scripts/debian/cloud-init-debian.sh
+++
@@ -1,6 +0,0 @@
-# Wheezy backports.
-echo 'deb http://repository.linux.pf/debian wheezy-backports main' >> /etc/apt/sources.list
-apt-get update
-
-# Install cloud-init.
-apt-get install -y cloud-init
--- scripts/debian/cloudinit.sh
+++ scripts/debian/cloudinit.sh
@@ -1,1 +1,6 @@
+# Wheezy backports.
+echo 'deb http://repository.linux.pf/debian wheezy-backports main' >> /etc/apt/sources.list
+apt-get update
+
+# Install cloud-init.
 apt-get install -y cloud-init
