48c91ca58116107e4770275955a5854644de0948
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-08-08 13:31:48 -1000

    Fix bug au boot de l'image et installation par défaut de curl.

--- scripts/ubuntu/base.sh
+++ scripts/ubuntu/base.sh
@@ -1,6 +1,10 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r)
+apt-get -y install curl
+
+sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+cat /root/.profile
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
@@ -8,3 +12,4 @@ sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 grep -qs autocd /root/.bashrc || echo "shopt -s autocd\nalias l='ls -al'" >>/root/.bashrc
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
